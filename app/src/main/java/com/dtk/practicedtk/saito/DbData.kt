be76efa5b067c91c3b89e349b260530bea44f0c9
saitetu <razikon@gmail.com>
2018-02-21 00:56:07 +0900

    [add]データベース用castクラス

---
+++ app/src/main/java/com/dtk/practicedtk/saito/DbData.kt
@@ -0,0 +1,14 @@
+package com.dtk.practicedtk.saito
+
+import java.time.Year
+
+/**
+ * Created by saitetu on 2018/02/20.
+ */
+data class DbData(
+        var id:Int,
+        var year:Int,
+        var month :Int,
+        var day:Int,
+        var ramen:Int
+        )
